340c212c0cb612d9168a14aac710a57895384ce1
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2023-02-10 19:36:15 -0300

    feat: first implementation

--- src/kyu5/moveZeros/index.ts
+++ src/kyu5/moveZeros/index.ts
@@ -1,4 +1,6 @@
-export default function moveZeros(arr) {
-  return null;
+export default function moveZeros(arr: Array<string|number|boolean>): Array<string|number|boolean> {
+  const zeros = arr.filter((el) => el === 0);
+  const nonZeros = arr.filter((el) => el !== 0);
+  return [...nonZeros, ...zeros];
 }
 
